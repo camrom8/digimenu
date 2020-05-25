dfc746729aff7591eb81b6525ad9a803ab34c7a1
camilo1rp <camilo1rp@gmail.com>
2020-05-24 20:42:55 -0500

    modified settings and nginx.conf

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myshop', 'myshop:8080']
+ALLOWED_HOSTS = ['digimenu', 'digimenu:8080']
 
 
 # Application definition
