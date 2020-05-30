9c64bcf5d04fb6601fbb13785f964ef06f6bab55
camilo1rp <camilo1rp@gmail.com>
2020-05-30 15:21:35 -0500

    clear cart if different menu
    Signed-off-by: camilo1rp <camilo1rp@gmail.com>

--- menus/views.py
+++ menus/views.py
@@ -84,6 +84,16 @@ class MenuDetails(DetailView):
     slug_url_kwarg = 'title_slug'
     slug_field = 'title_slug'
 
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        cart = Cart(request)
+        for item in cart:
+            if item['product'].item.menu != self.object:
+                cart.clear()
+            break
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
     def get_context_data(self, **kwargs):
         """get categories in the context data"""
         context = super().get_context_data(**kwargs)
