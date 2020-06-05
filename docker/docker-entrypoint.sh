07ff58573e67f6687fb70f1d82fb750a921ced21
camilo1rp <camilo1rp@gmail.com>
2020-06-04 19:24:16 -0500

    added s3 storage and added env variables to docker
    Signed-off-by: camilo1rp <camilo1rp@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,6 +8,9 @@ export DB_PASSWORD=$(cat /run/secrets/db_password)
 export SECRET_KEY=$(cat /run/secrets/secret_key)
 export EMAIL_HOST_USER=$(cat /run/secrets/email_host_user)
 export EMAIL_HOST_PASSWORD=$(cat /run/secrets/email_host_password)
+export AWS_ACCESS_KEY_ID=$(cat /run/secrets/aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/aws_secret_access_key)
+export AWS_STORAGE_BUCKET_NAME=$(cat /run/secrets/aws_storage_bucket_name)
 
 
 # run db migrations (retry on error)
--- myshop/settings.py
+++ myshop/settings.py
@@ -131,9 +131,32 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+# USE_S3 = os.getenv('USE_S3') == 'TRUE'
+USE_S3 = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', 'digimenu-bucket')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'myshop.storage_backends.StaticStorage'
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'myshop.storage_backends.PublicMediaStorage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
+SASS_PRECISION = 8
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -141,8 +164,6 @@ STATICFILES_FINDERS = [
 ]
 # Django Sass
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'index/static/scss')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # key for storing cart info.
 CART_SESSION_ID = 'cart'
